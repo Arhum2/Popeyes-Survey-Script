fbb6aa86708540fe35ce99eff451e8894a313845
Arhum2 <arhumshahzad2003@gmail.com>
2022-07-21 03:02:00 -0400

    updated to work with latest site version

--- popeyes.py
+++ popeyes.py
@@ -12,46 +12,50 @@ browser.get("https://www.tellpopeyes.com/")
 time.sleep(2)
 
 # Navigating to form
-next_btn = browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/form/div/div[1]/input')
+next_btn = browser.find_element(By.XPATH, '/html/body/div[3]/div/form/div/div[2]/div[2]/div[3]/div[2]/input')
 next_btn.click()
 
 # Setting store number
-store_num = browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/form/div/p[5]/input')
+time.sleep(3)
+store_num = browser.find_element(By.XPATH, '/html/body/div[3]/div/form/div/div[2]/div[2]/div[3]/div[1]/div[5]/div[3]/div/fieldset/div/div/table/tbody/tr[1]/td[2]/input')
 store_num.click()
 store_num.send_keys('11962')
 
 # Setting date
-date_month = browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/form/div/p[7]/select[1]')
-date_month.send_keys("1")
+date_month = browser.find_element(By.XPATH, '/html/body/div[3]/div/form/div/div[2]/div[2]/div[3]/div[1]/div[5]/div[3]/div/fieldset/div/div/table/tbody/tr[2]/td[2]/input')
 date_month.click()
-
-
-date_day = browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/form/div/p[7]/select[2]')
-date_day.send_keys("1")
-date_day.click()
+time.sleep(2)
+days = browser.find_element(By.TAG_NAME, 'td')
+days.click()
 
 # Setting time
-hour = browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/form/div/p[9]/select[1]')
-hour.send_keys("1")
+hour = browser.find_element(By.XPATH, '/html/body/div[3]/div/form/div/div[2]/div[2]/div[3]/div[1]/div[7]/div[3]/div/fieldset/fieldset/div/table/tbody/tr/td[3]/select')
+hour.click()
+hour.send_keys(Keys.ARROW_UP)
+hour.send_keys(Keys.ARROW_UP)
 hour.click()
 
-min = browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/form/div/p[9]/select[2]')
-min.send_keys("1")
+min = browser.find_element(By.XPATH, '/html/body/div[3]/div/form/div/div[2]/div[2]/div[3]/div[1]/div[7]/div[3]/div/fieldset/fieldset/div/table/tbody/tr/td[6]/select')
 min.click()
+min.send_keys("0")
 
-period = browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/form/div/p[9]/select[3]')
+period = browser.find_element(By.XPATH, '/html/body/div[3]/div/form/div/div[2]/div[2]/div[3]/div[1]/div[7]/div[3]/div/fieldset/fieldset/div/table/tbody/tr/td[9]/select')
 period.send_keys("1")
 period.send_keys(Keys.ARROW_DOWN)
+period.send_keys(Keys.ARROW_DOWN)
+period.click()
 
 # Setting amount
-amount = browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/form/div/p[11]/input')
+amount = browser.find_element(By.XPATH, '/html/body/div[3]/div/form/div/div[2]/div[2]/div[3]/div[1]/div[9]/div[3]/div/fieldset/div/div/table/tbody/tr/td[2]/input')
 amount.send_keys('10')
 
+
+time.sleep(10)
 # Submitting form
-submit = browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/form/div/div[1]/input')
+submit = browser.find_element(By.XPATH, '/html/body/div[3]/div/form/div/div[2]/div[2]/div[3]/div[2]/input[2]')
 submit.click()
 
-time.sleep(2)
+
 
 next = browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[2]/form/div/div[3]/input')
 next.click()
